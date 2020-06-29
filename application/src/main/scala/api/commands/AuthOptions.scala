34e0a5596c781f76c134f5c4ca58402e4c08da91
James Santucci <james.santucci@gmail.com>
2020-06-29 13:58:37 -0500

    document what happens with multiple auth specifications

--- application/src/main/scala/api/commands/AuthOptions.scala
+++ application/src/main/scala/api/commands/AuthOptions.scala
@@ -6,9 +6,21 @@ import com.jisantuc.configableauth.datamodel.Domain
 trait AuthOptions {
 
   private val authedDomains =
-    Opts
-      .options[Domain]("authed-domain", help = "Which sorts of actions to require authentication for")
-      .orEmpty
+    (Opts
+      .options[Domain](
+        "authed-domain",
+        help =
+          "Which sorts of actions to require authentication for. Excludes --auth-all as a valid option if provided."
+      )
+      .orEmpty)
+      .orElse(
+        Opts
+          .flag("auth-all", help = "Authenticate all routes. Excludes --authed-domain as valid options if provided.")
+          .orFalse map {
+          case true => List(Domain.Read, Domain.Destructive, Domain.Creative)
+          case _    => Nil
+        }
+      )
 
   val authConfig: Opts[AuthConfig] = authedDomains map { domains =>
     AuthConfig(domains.toSet)
