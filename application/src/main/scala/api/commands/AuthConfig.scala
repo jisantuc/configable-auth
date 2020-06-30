6548213f56d0c27f793972413ea0cb6dd6d41656
James Santucci <james.santucci@gmail.com>
2020-06-30 16:51:56 -0500

    accept auth service params at startup

--- application/src/main/scala/api/commands/AuthConfig.scala
+++ application/src/main/scala/api/commands/AuthConfig.scala
@@ -1,7 +1,22 @@
 package com.jisantuc.configableauth.api.commands
 
 import com.jisantuc.configableauth.datamodel.Domain
+import eu.timepit.refined.types.numeric.PosInt
+import eu.timepit.refined.types.string.NonEmptyString
 
 final case class AuthConfig(
-    authedDomains: Set[Domain]
+    authedDomains: Set[Domain],
+    authenticationServiceConfig: Option[AuthenticationServiceConfig]
 )
+
+final case class AuthenticationServiceConfig(
+    authenticationHost: NonEmptyString,
+    authenticationPort: PosInt,
+    authenticationRoute: Option[String],
+    authenticationScheme: String
+) {
+
+  val authServiceRoute = s"""$authenticationScheme://$authenticationHost${authenticationRoute map { s =>
+    s"/$s"
+  } getOrElse ""}"""
+}
